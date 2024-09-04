c1999ec6488d42b561478151822b15a10d3e997b
aheroglu <ahmet20033134@gmail.com>
2024-09-04 12:07:00 +0300

    Create metodu eklendi

--- Program.cs
+++ Program.cs
@@ -15,10 +15,16 @@ app.UseSwagger();
 app.UseSwaggerUI();
 
 app.MapGet("/", () => "Hello World!");
+
 app.MapGet("/getall", () => Results.Ok(new List<string>()
 {
     "Example 1",
     "Example 2"
 }));
+
+app.MapGet("/create", (string work) =>
+{
+    Results.Ok(work);
+});
 
 app.Run();
